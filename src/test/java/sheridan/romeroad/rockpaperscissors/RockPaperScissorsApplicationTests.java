b3283040e52ba41bfd0029d14c78de7262909a8a
David Romero <romeroad@sheridancollage.ca>
2021-02-01 21:06:38 -0500

    Initial commit

---
+++ src/test/java/sheridan/romeroad/rockpaperscissors/RockPaperScissorsApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.romeroad.rockpaperscissors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockPaperScissorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
