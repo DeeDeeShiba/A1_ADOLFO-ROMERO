e8feb5c85125c16decfbfacbf50c314c40a3dbbd
David Romero <romeroad@sheridancollage.ca>
2021-02-03 01:26:35 -0500

    Added Methods and functionality to RockPaperScissorController, must work on Envelope class and define it

--- src/main/java/sheridan/romeroad/rockpaperscissors/controller/RockPaperScissorController.java
+++ src/main/java/sheridan/romeroad/rockpaperscissors/controller/RockPaperScissorController.java
@@ -1,4 +1,28 @@
 package sheridan.romeroad.rockpaperscissors.controller;
 
+import org.springframework.stereotype.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class RockPaperScissorController {
+    private final Logger logger = LoggerFactory.getLogger(RockPaperScissorController.class);
+
+    @GetMapping(value={"/","Input"})
+    public String Input(){
+        logger.trace("Input is called");
+        return "Input";
+    }
+
+    @GetMapping("/process")
+    public ModelAndView process(@ModelAttribute Envelope envelope){
+        logger.trace("process() is called");
+        logger.debug("envelope = "+envelope);
+        return new ModelAndView("Output");
+    }
+
+
 }
