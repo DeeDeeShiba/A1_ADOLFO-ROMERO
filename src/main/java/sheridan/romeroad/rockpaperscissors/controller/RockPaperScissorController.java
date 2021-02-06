f6661f12a396ddfc92380016229dabe249edcd38
David Romero <romeroad@sheridancollage.ca>
2021-02-05 23:05:07 -0500

    Finishing touches

--- src/main/java/sheridan/romeroad/rockpaperscissors/controller/RockPaperScissorController.java
+++ src/main/java/sheridan/romeroad/rockpaperscissors/controller/RockPaperScissorController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import sheridan.romeroad.rockpaperscissors.domain.Envelope;
+//import sheridan.romeroad.rockpaperscissors.domain.Envelope;
 import sheridan.romeroad.rockpaperscissors.domain.Game;
 
 @Controller
@@ -15,20 +15,30 @@ public class RockPaperScissorController {
 
     private final Logger logger = LoggerFactory.getLogger(RockPaperScissorController.class);
 
-    @GetMapping(value = {"/", "Input"})
+    /*@GetMapping(value = {"/", "Input"})
     public String input() {
         logger.trace("input() is called");
+
         return "Input";
 
+    }*/
+    @GetMapping(value={"/Output"})
+    public ModelAndView game (@RequestParam(defaultValue = "0")int option){
+        logger.trace("game() is called");
+        Game game = new Game(option);
+        game.decideWinner();
+        return new ModelAndView("Output", "game", game);
     }
 
 
-    @GetMapping("/Process")
+    /*@GetMapping("/Process")
     public ModelAndView process(@ModelAttribute Envelope envelope) {
         logger.trace("process() is called");
         logger.debug("envelope = " + envelope);
         return new ModelAndView("Output", "envelope", envelope);
-    }
+    }*/
+
+
 }
 
 
--- src/main/java/sheridan/romeroad/rockpaperscissors/domain/Game.java
+++ src/main/java/sheridan/romeroad/rockpaperscissors/domain/Game.java
@@ -1,49 +1,58 @@
 package sheridan.romeroad.rockpaperscissors.domain;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sheridan.romeroad.rockpaperscissors.controller.RockPaperScissorController;
+
 import java.io.Serializable;
 
 public class Game implements Serializable {
 
-    private int computerRandChoice;
-    private char userChoice;
-    private String outcome;
+    private final Logger logger = LoggerFactory.getLogger(RockPaperScissorController.class);
 
-    public Game(char choice) {
-        int randChoice = (int) (4 * Math.random());
-        computerRandChoice = randChoice;
+    public int computerRandChoice;
+    public int userChoice;
+    public String outcome;
+
+    public Game(int choice) {
+        logger.trace("Game() is called");
+        computerRandChoice = (int) (4 * Math.random());
         userChoice = choice;
-
     }
     public String getOutcome(){return outcome;}
 
-    public void decideWinnner() {
+    public void decideWinner() {
+        logger.trace("decideWinner() is called");
         //draw event
         if (userChoice == computerRandChoice) {
-            outcome = "DRAW";
+            this.outcome = "DRAW";
         }
         //rock v paper - user lost
         else if (userChoice == '0' && computerRandChoice == '1') {
-            outcome = "YOU LOOSE";
+            this.outcome = "YOU LOOSE";
         }
         //rock v paper - user won
         else if (userChoice == '1' && computerRandChoice == '0') {
-            outcome = "YOU WIN";
+            this.outcome = "YOU WIN";
         }
         //rock v scissor - user lost
         else if (userChoice == '2' && computerRandChoice == '0') {
-            outcome = "YOU LOOSE";
+            this.outcome = "YOU LOOSE";
         }
         //rock v scissor - user wins
         else if (userChoice == '0' && computerRandChoice == '2') {
-            outcome = "YOU WIN";
+            this.outcome = "YOU WIN";
         }
         //scissor v paper - user lost
         else if (userChoice == '1' && computerRandChoice == '2') {
-            outcome = "YOU LOOSE";
+            this.outcome = "YOU LOOSE";
         }
         //scissor v paper - user win
         else if (userChoice == '2' && computerRandChoice == '1') {
-            outcome = "YOU WIN";
+            this.outcome = "YOU WIN";
+        }
+        else{
+            this.outcome = "ERROR";
         }
 
     }
